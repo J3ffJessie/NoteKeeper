0b8b1baddb41be00f24e6a95bf051e6f3bc5490c
J3ffJessie <jejess6125@gmail.com>
2020-01-30 17:46:34 -0600

    First commit of the NoteKeeper React app

---
+++ src/components/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+import notes from "../notes.js";
+
+
+function createNote(notes) {
+  return (
+    <Note
+    key={notes.key}
+    title={notes.title}
+    content={notes.content}
+    />
+  );
+}
+
+
+
+function App(){
+  return (
+    <div>
+    <Header />
+    {notes.map(createNote)}
+    <Footer />
+    </div>
+  );
+}
+
+
+export default App;
